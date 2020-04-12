414d593c30a07f901a768817a0fd67b8275b0acf
your username <daniel@danielrosehill.co.il>
2020-04-12 16:11:24 +0300

    Changed commands from concurrent to sequential

--- Start_Shtisel.sh
+++ Start_Shtisel.sh
@@ -3,20 +3,20 @@
 # By: Daniel Rosehill / DanielRosehill.co.il
 #
 # Call arandr script to pipe output to HDMI (TV)
-/bin/bash /home/daniel/TV_only.sh &
+/bin/bash /home/daniel/TV_only.sh 
 # Unmuting volume and then setting it to 100%
-amixer sset Master unmute &
-amixset sset Master 100% &
+amixer sset Master unmute 
+amixset sset Master 100% 
 # Open 'Shtisel' series page in Netflix
-echo "Opening Shtisel page on Netlix" &
-google-chrome https://www.netflix.com/title/81004164 &
-sleep 5 &
+echo "Opening Shtisel page on Netlix" 
+google-chrome https://www.netflix.com/title/81004164 
+sleep 5 
 # Click the resume button
-xdotool mousemove 115 531 &
-xdotool click 1 &
+xdotool mousemove 115 531 
+xdotool click 1 
 # Wait five seconds and then make full screen
-sleep 5 &
-xdotool mousemove 1857 991 &
+sleep 5 
+xdotool mousemove 1857 991 
 at now +300 minutes <<< "sudo poweroff"
 exit
 
