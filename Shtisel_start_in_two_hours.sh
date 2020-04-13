5ea8c8a2833db2f0f50f1085017906c35888df44
your username <daniel@danielrosehill.co.il>
2020-04-13 12:09:22 +0300

    Two hour delay script

---
+++ Shtisel_start_in_two_hours.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# A bash script for launching 'Shtisel' in Netflix
+# By: Daniel Rosehill / DanielRosehill.co.il
+#
+# Call arandr script to pipe output to HDMI (TV)
+sleep 2h
+/bin/bash /home/daniel/TV_only.sh 
+# Unmuting volume and then setting it to 100%
+amixer sset Master unmute 
+amixset sset Master 100% 
+# Open 'Shtisel' series page in Netflix
+echo "Opening Shtisel page on Netlix" 
+google-chrome https://www.netflix.com/title/81004164 
+sleep 5 
+# Click the resume button
+xdotool mousemove 115 531 
+xdotool click 1 
+# Wait five seconds and then make full screen
+sleep 5 
+xdotool mousemove 1857 991 
+sleep 2h
+sudo poweroff
+exit
+
+
+
